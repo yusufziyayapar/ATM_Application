3e7edbbf694f3e4cff7e86ce9e0977d89eee509e
yusufziyayapar <yusufcrawlziya@gmail.com>
2022-09-19 20:08:17 +0300

    .

---
+++ build/CMakeFiles/ATM.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "ATM"
+  "ATM.pdb"
+  "CMakeFiles/ATM.dir/main.c.o"
+  "CMakeFiles/ATM.dir/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ATM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
